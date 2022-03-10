a3802e9c7306f9f850388c7cf16b0a52623431d2
Jade Rigby <jaderigby@PL-Jade-Rigby.local>
2022-03-09 17:40:15 -0700

    seperating of suffix pattern to one variable

--- helpers.py
+++ helpers.py
@@ -157,10 +157,15 @@ def handle_svgo():
 
 	return status
 
+#======================
+# SHARED SUFFIX PATTERN
+#======================
+sharedSuffixPat = '(.designer.svg|.ink.svg|.sketch.svg|.formatted.svg|.clean.svg|.minified.svg|.svg|.serif-clean.svg)'
+
 def clean(FROM, TO, FILE, TYPE):
 	import re
 	#= remove previous extensions, where possible:
-	svgFileCore = re.sub('(.designer.svg|.ink.svg|.sketch.svg|.formatted.svg|.clean.svg|.minified.svg|.svg|.serif-clean.svg)', '', FILE)
+	svgFileCore = re.sub(sharedSuffixPat, '', FILE)
 
 	if TYPE == 'pretty':
 		run_command("svgo '{FROM}/{FILE}' -o '{TO}/{FILE_CORE}.clean.svg' --pretty".format(FROM = FROM.replace('\n', ''), TO = TO.replace('\n', ''), FILE = FILE, FILE_CORE = svgFileCore))
@@ -185,7 +190,7 @@ def format(FROM, TO, FILE, STROKES_TO_PATH = True):
 	import re
 
 	#= remove previous extensions, where possible:
-	svgFileCore = re.sub('(.designer.svg|.ink.svg|.sketch.svg|.formatted.svg|.clean.svg|.minified.svg|.svg|.serif-clean.svg)', '', FILE)
+	svgFileCore = re.sub(sharedSuffixPat, '', FILE)
 	
 	run_command('scp {FROM}/{FILE} {TO}/{FILE_CORE}.formatted.svg'.format(FROM = FROM.replace('\n', ''), TO = TO.replace('\n', ''), FILE = FILE, FILE_CORE = svgFileCore))
 
@@ -212,7 +217,7 @@ def serif_cleanup(FROM, TO, FILE):
 	import re
 
 	#= remove previous extensions, where possible:
-	svgFileCore = re.sub('(.designer.svg|.ink.svg|.sketch.svg|.formatted.svg|.clean.svg|.minified.svg|.svg|.serif-clean.svg)', '', FILE)
+	svgFileCore = re.sub(sharedSuffixPat, '', FILE)
 
 	CONTENT = read_file('{FROM}/{FILE_CORE}.svg'.format(FROM = FROM.replace('\n', ''), FILE_CORE = svgFileCore))
 	CONTENT_REFINED = re.sub('serif:id="[a-z A-Z0-9]*"', '', CONTENT)
